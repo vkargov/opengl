3195996fb8a90f0e8d5afe8e03d2e754ed604619
Vladimir Kargov <kargov@gmail.com>
2020-01-06 12:26:44 +0300

    finish unit 4, move out some functionality outside

---
+++ 4 Textures/vgl.h
@@ -0,0 +1,42 @@
+/* vova gl, functions generalizing repetitive actions */
+
+#include <iostream>
+
+GLuint vglLoadTexture(const char* path, const char* sampler_name, GLuint shader_program, GLuint texture_unit, GLenum format) {
+  // prepare our texture
+  GLuint texture;  
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+  stbi_set_flip_vertically_on_load(true);
+  if (!data) {
+    std::cerr << "vglLoadTexture oof: cannot load texture " << path << '\n';
+    throw std::invalid_argument{path};
+  }
+  //std::cout << "Image loaded with width = " << width << " and height = " << height << '\n';
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  stbi_image_free(data);
+
+  glUseProgram(shader_program);
+  GLint texture_unit_location;
+  texture_unit_location = glGetUniformLocation(shader_program, sampler_name);
+  if (texture_unit_location < 0) {
+    if (texture_unit_location == -1) {
+      std::cerr << "vglLoadTexture oof: couldn't find find " << sampler_name << ".\n";
+    }
+    std::cerr << "glGetUniformLocation oof\n";
+    return 1;
+  }
+  glUniform1i(texture_unit_location, texture_unit);
+
+  return texture;
+}
